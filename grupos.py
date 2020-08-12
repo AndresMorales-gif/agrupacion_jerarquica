83467bce57f3b7af2bd6db6adac103238f796362
Andres Morales <Andresmorales.fuac@gmail.com>
2020-08-12 10:46:09 -0500

    Clase grupo creada

---
+++ grupos.py
@@ -0,0 +1,8 @@
+class Grupo(object):
+	def __init__(self, elemento_1, elemento_2)
+		self.elemento_1 = elemento_1
+		self.elemento_2 = elemento_2
+
+	def get_grupo(self):
+		return (self.elemento_1, self.elemento_2) 
+		
